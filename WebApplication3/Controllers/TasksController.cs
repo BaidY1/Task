d3b66e6d384f48dd95da7239248f0b58d94cb4f0
BaidY <BaidY@DESKTOP-MSTHTOD>
2022-06-13 01:26:17 +0500

    Add project files.

---
+++ WebApplication3/Controllers/TasksController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Data;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class TasksController : Controller
+    {
+        private WebApplication3Context db = new WebApplication3Context();
+
+      
+        public ActionResult Index()
+        {
+            return View(db.Tasks.ToList());
+        }
+
+     
+       
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormCollection collection)
+        {
+            var task = new Task();
+            task.TaskName = collection["TaskName"];
+            task.TaskDescription = collection["TaskDescription"];
+            task.TaskStatus = collection["TaskStatus"];
+            
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(task);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication3/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string TaskStatus { get; set; }
+    }
+}
